e6e2bbcbcf905c312e7ba6587a112f0bbbe8c424
Aarsh Dave <aarshdevangkuma.dave@mavs.uta.edu>
2018-04-11 22:03:02 -0500

    Optimized code according to encoder

--- Python/Main_code/Main_motor.py
+++ Python/Main_code/Main_motor.py
@@ -9,7 +9,8 @@ class Motor():
     #Initialize Motor class
     def __init__(self):
         self.PWM_pin = 12 #GPIO 12 on board pin 32
-        self.PWM_frequency = 30000
+        self.debug_pin = 4
+        self.PWM_frequency = 20000
         self.duty_cycle = 100
         self.input_duty_cycle = self.duty_cycle * 10000
         self.pi = pigpio.pi()
@@ -24,35 +25,45 @@ class Motor():
 
         #GPIO initialization
         self.pi.set_mode(self.PWM_pin, pigpio.OUTPUT)
+        self.pi.set_mode(self.debug_pin, pigpio.OUTPUT)
+        self.pi.write(self.debug_pin, 0)
+        self.pi.hardware_PWM(self.PWM_pin, self.PWM_frequency, 0)
+        time.sleep(2)
+        print 'Starting at 100%'
         self.pi.hardware_PWM(self.PWM_pin, self.PWM_frequency, self.input_duty_cycle)
-        time.sleep(1)
-        oldValue = 625
 
+        for tr in range(1,5):
+            self.maxRPM = self.connect.readline()
+            self.maxRPM = re.findall(r"[-+]?\d*\.\d+|\d+", self.maxRPM)
+            self.maxRPM = int(self.maxRPM[0])
+        print 'Ready to go!!!'
+        self.pi.write(self.debug_pin, 1)
         
     def mymap(self, x, in_min, in_max, out_min, out_max):
         return (x - in_min)*(out_max - out_min)/(in_max - in_min)+out_min
 
     def Run(self):
-        oldValue = 625
+        self.oldValue = self.maxRPM
         while True:
             uart_data = self.connect.readline()
+            print uart_data
             list_of_data = re.findall(r"[-+]?\d*\.\d+|\d+", uart_data)
             encoderRPM = int(list_of_data[0])
             current = float(list_of_data[1])
-            power = float(list_of_data[2])
-            #print upload_RPM , '\t' , upload_current , '\t' , upload_power
+            #power = float(list_of_data[2])
+            # print encoderRPM , '\t' , current , '\t' , power
             self.fb.put('/Current_Motor_Readings','Current', current)
             self.fb.put('/Current_Motor_Readings','encoderRPM', encoderRPM)
-            self.fb.put('/Current_Motor_Readings','Power', power)
+            #self.fb.put('/Current_Motor_Readings','Power', power)
     
             userRPM = self.fb.get('/Current_Motor_Readings/userRPM', None)
-            userDutyCycle = int(round(self.mymap(userRPM, 32, 625, 5, 100)))
+            userDutyCycle = int(round(self.mymap(userRPM, 0, self.maxRPM, 5, 100)))
+            print 'Min: ', self.minRPM, ' Max: ', self.maxRPM
             userDutyCycle = userDutyCycle * 10000
-            print userDutyCycle
-            if(oldValue != userRPM):
+            if(self.oldValue != userRPM):
                 self.pi.hardware_PWM(self.PWM_pin, self.PWM_frequency, userDutyCycle)
                 print 'Gayo andar!'
-            oldValue = userRPM
+            self.oldValue = userRPM
 
 if __name__ == "__main__":
     Motor_object = Motor()
