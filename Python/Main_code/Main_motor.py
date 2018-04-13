1d3a4100a9c679dd8d5b794cb6263a6241488d40
Aarsh Dave <aarshdevangkuma.dave@mavs.uta.edu>
2018-04-13 13:41:34 -0500

    Working PID with on-off switch

--- Python/Main_code/Main_motor.py
+++ Python/Main_code/Main_motor.py
@@ -28,42 +28,82 @@ class Motor():
         self.pi.set_mode(self.debug_pin, pigpio.OUTPUT)
         self.pi.write(self.debug_pin, 0)
         self.pi.hardware_PWM(self.PWM_pin, self.PWM_frequency, 0)
-        time.sleep(2)
+        time.sleep(1)
         print 'Starting at 100%'
         self.pi.hardware_PWM(self.PWM_pin, self.PWM_frequency, self.input_duty_cycle)
 
-        for tr in range(1,5):
+        for tr in range(1,3):
             self.maxRPM = self.connect.readline()
             self.maxRPM = re.findall(r"[-+]?\d*\.\d+|\d+", self.maxRPM)
             self.maxRPM = int(self.maxRPM[0])
         print 'Ready to go!!!'
         self.pi.write(self.debug_pin, 1)
+        self.d_error = 0
+        self.i_error = 0
+        self.feedRPM = 0
+        self.kp = 0.05
+        self.kd = 0.03
+        self.ki = 0.0000
         
     def mymap(self, x, in_min, in_max, out_min, out_max):
         return (x - in_min)*(out_max - out_min)/(in_max - in_min)+out_min
 
     def Run(self):
         self.oldValue = self.maxRPM
+        #while True:
+            #self.switch = self.fb.get('/Current_Motor_Readings/Switch', None)
+            #print self.switch
         while True:
-            uart_data = self.connect.readline()
-            print uart_data
-            list_of_data = re.findall(r"[-+]?\d*\.\d+|\d+", uart_data)
-            encoderRPM = int(list_of_data[0])
-            current = float(list_of_data[1])
-            #power = float(list_of_data[2])
-            # print encoderRPM , '\t' , current , '\t' , power
-            self.fb.put('/Current_Motor_Readings','Current', current)
-            self.fb.put('/Current_Motor_Readings','encoderRPM', encoderRPM)
-            #self.fb.put('/Current_Motor_Readings','Power', power)
-    
-            userRPM = self.fb.get('/Current_Motor_Readings/userRPM', None)
-            userDutyCycle = int(round(self.mymap(userRPM, 0, self.maxRPM, 5, 100)))
-            print 'Min: ', self.minRPM, ' Max: ', self.maxRPM
-            userDutyCycle = userDutyCycle * 10000
-            if(self.oldValue != userRPM):
-                self.pi.hardware_PWM(self.PWM_pin, self.PWM_frequency, userDutyCycle)
-                print 'Gayo andar!'
-            self.oldValue = userRPM
+            #Read data from serial port and extract it
+            self.uart_data = self.connect.readline()
+            print self.uart_data
+            self.list_of_data = re.findall(r"[-+]?\d*\.\d+|\d+", self.uart_data)
+            self.encoderRPM = int(self.list_of_data[0])
+            self.current = float(self.list_of_data[1])
+            self.power = float(self.list_of_data[2])
+
+            #Read and write data to and from firebase
+            self.fb.put('/Current_Motor_Readings','Current', self.current)
+            self.fb.put('/Current_Motor_Readings','encoderRPM', self.encoderRPM)
+            self.fb.put('/Current_Motor_Readings','Power', self.power)
+            self.userRPM = self.fb.get('/Current_Motor_Readings/userRPM', None)
+            self.PID_Status = self.fb.get('/Current_Motor_Readings/PID', None)
+
+            #Debugging
+            print 'Min: 0' + ' Max: ', self.maxRPM
+
+            self.userDutyCycle = int(round(self.mymap(self.userRPM, 0, self.maxRPM, 5, 100)))
+            
+            #PID off condition
+            if not self.PID_Status:
+                self.OpenLoop()
+
+            #PID on condition
+            if self.PID_Status:
+                self.CloseLoop()
+                
+            #self.pi.hardware_PWM(self.PWM_pin, self.PWM_frequency, 0)
+
+    def OpenLoop(self):
+        self.userDutyCycle = self.userDutyCycle * 10000
+        if(self.oldValue != self.userRPM):
+            self.pi.hardware_PWM(self.PWM_pin, self.PWM_frequency, self.userDutyCycle)
+            print 'PID off : Gayo andar!'
+        self.oldValue = self.userRPM
+
+    def CloseLoop(self):
+        self.p_error = self.userRPM - self.encoderRPM
+        self.feedRPM = self.feedRPM + (self.p_error*self.kp) + (self.d_error*self.kd) + (self.i_error*self.ki)
+        self.feedDutyCycle = int(round(self.mymap(self.feedRPM, 0, self.maxRPM, 5, 100)))
+        
+        self.feedDutyCycle = self.feedDutyCycle * 10000 + self.userDutyCycle * 10000
+        print 'feed dutycycle: ',  self.feedDutyCycle/10000, 'FeedRPM : ', self.feedRPM
+        if(self.feedDutyCycle > 0 and self.feedDutyCycle < 1000000):
+            self.pi.hardware_PWM(self.PWM_pin, self.PWM_frequency, self.feedDutyCycle)
+            print 'PID on: Gayo andar!'
+
+        self.d_error = self.p_error
+        self.i_error = self.i_error + self.d_error
 
 if __name__ == "__main__":
     Motor_object = Motor()
